6963c2d5fffc7333872547666925dafcb2691ddd
Ruth Rabo Magaji <ruthrabs22@gmail.com>
2023-03-17 16:29:02 -0700

    5-printf.c

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+ * main - a program that uses the printf function to print a line
+ *
+ * Return: always o (success)
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n")
+		return (0);
+}
