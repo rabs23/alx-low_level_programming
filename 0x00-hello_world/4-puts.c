1320f398f428ff7065803f4ba4324a2fda29aec4
Ruth Rabo Magaji <ruthrabs22@gmail.com>
2023-03-17 07:40:37 -0700

    4-puts.c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+ * main - A program that prints with put function
+ *
+ * Return: Always 0 (success)
+ */
+int main(void)
+{
+puts("\"Programming is like building a multilingual puzzle")
+	return (0);
+}
