a24eb908bad4fbb0921c420ab50069ab1f0a94d5
Ruth Rabo Magaji <ruthrabs22@gmail.com>
2023-03-24 01:58:45 -0700

    1-main.c

--- 0x03-debugging/2-largest_number.c
+++ 0x03-debugging/2-largest_number.c
@@ -16,20 +16,20 @@ int largest_number(int a, int b, int c)
 	{
 		largest = a;
 	else if (a > c && c > b)
-		largest = a;
 	}
+		largest = a;
 	else if (b > a && a > c)
 	{
 		largest = b;
-	}
 	else if (b > c && c > a)
+	}
 		largest = b;
 	else if (c > a && a > b)
+	{
 
 		largest = c;
 
 	else if (c > b && b > a)
-	{
 		largest = c;
 	}
 
