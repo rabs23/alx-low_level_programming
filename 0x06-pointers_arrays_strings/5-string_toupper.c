147a959124bfe9a7713e66284d6aea091c16bc25
Ruth Rabo Magaji <ruthrabs22@gmail.com>
2023-03-31 17:46:33 -0700

    update

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * main - a function that changes alllowercase letters of a string to uppercase
+ * @n: pointer
+ * Return: char
+ */
+
+char *string_toupper(char *n)
+{
+	int i;
+
+	i = 0;
+	while (n[i] != '\0')
+	{
+		if (n[i] >= 'a' && n[i] <= 'z')
+			n[i] = n[i] - 32;
+		i++;
+	}
+	return (n);
+}
