e38afb1e1bdedac53126845949ac33c9db0c1ea0
Ruth Rabo Magaji <ruthrabs22@gmail.com>
2023-04-04 00:28:55 -0700

    update

---
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * main - a function that fills a memory with a constant byte
+ * @n: bytes
+ * @s: pointed direction
+ * @b: constant byte
+ * Return: 0
+ */
+
+char *_memset(char *s, char b, unsigned int n)
+{
+	unsigned int i;
+
+	for (i = 0; i < n; i++)
+		s[i] = b;
+	return (s);
+}
