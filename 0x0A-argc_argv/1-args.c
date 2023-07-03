2607d06c6f47aebe46a67cd8ada28a3c06c95202
rabs23 <ruthrabs22@gmail.com>
2023-07-03 06:52:14 -0700

    updated

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - a program that prints the number of arguments passed to it
+ * @argc: argument string
+ * @argv: argument pointer
+ * Return: 0
+ */
+
+int main(int argc, char **argv __attribute__((unused)))
+{
+	printf("%d\n", argc - 1);
+
+	return (0);
+}
