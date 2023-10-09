d5c88bb8eec524517db507acdc2360875b9ee789
Ruth Rabo <ruthrabs22@gmail.com>
2023-10-08 18:56:47 -0700

    update

--- 0x0C-more_malloc_free/3-array_range.c
+++ 0x0C-more_malloc_free/3-array_range.c
@@ -9,5 +9,19 @@
 
 int *array_range(int min, int max)
 {
+	int j, s;
+	int *k;
 
+	if (min > max)
+		return (NULL);
+	s = max - min + 1;
+	k = malloc(sizeof(int) * s);
+	if (k == NULL)
+		return (NULL);
+	for (j = 0; j < s; j++)
+	{
+		k[j] = min;
+		min++;
+	}
+	return (k);
 }
