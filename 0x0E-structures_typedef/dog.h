d086b25c5e1daf6c2e9101595be104f0b827bbda
Ruth Rabo <ruthrabs22@gmail.com>
2023-10-10 06:56:49 -0700

    update

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,19 @@
+#ifndef DOG_H
+#define DOG_H
+/**
+ * struct dog - function that defines struct dog
+ * @name: name of the dog
+ * @age: age of the dog
+ * @owner: owner of the dog
+ *
+ * description: first task on structures
+ */
+
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+#endif
