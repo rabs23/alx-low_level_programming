6beb3270ad9a5c03e9b1ba5a2db20a5a71b305d2
rabs23 <ruthrabs22@gmail.com>
2023-10-30 17:40:40 -0700

    correction

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -3,7 +3,7 @@
 /**
  * create_file - function that creates a file
  * @filename: name of the file to be created
- * @text_content: contents of the file to be created
+ * @text_content: NULL terminated string to be added to the end of the file
  * Return: 1 on success, -1 on failure
  */
 
@@ -17,7 +17,7 @@ int create_file(const char *filename, char *text_content)
 		text_content = "";
 	for (f = 0; text_content[reading]; reading++)
 		;
-	writing = write(reading, text_content, f);
+	writing = write(f, reading, text_content);
 	if (writing == -1)
 		return (-1);
 	close(f);
