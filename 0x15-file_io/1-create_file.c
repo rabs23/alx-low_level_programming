30cb70879db42113b212ff3723d0ba4c3bd0f9f3
rabs23 <ruthrabs22@gmail.com>
2023-10-30 17:13:15 -0700

    update

---
+++ 0x15-file_io/1-create_file.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * create_file - function that creates a file
+ * @filename: name of the file to be created
+ * @text_content: contents of the file to be created
+ * Return: 1 on success, -1 on failure
+ */
+
+int create_file(const char *filename, char *text_content)
+{
+	int f = 0, reading, writing;
+
+	if (filename == NULL)
+		return (-1);
+	if (text_content == NULL)
+		text_content = "";
+	for (f = 0; text_content[reading]; reading++)
+		;
+	writing = write(reading, text_content, f);
+	if (writing == -1)
+		return (-1);
+	close(f);
+	return (1);
+}
